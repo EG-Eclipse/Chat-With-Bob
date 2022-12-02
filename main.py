be4aeae517c47157636421b38ef1fe00907008b2
Allen Davies <116924367+EG-Eclipse@users.noreply.github.com>
2022-12-02 14:26:04 +0000

    Verision: Beta 0.2

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 print('Hello Friend! Whats your name?')
 Name=input('Name: ')
-print('Hello', Name, ' What would you like to call me?')
-CompName=input('Whats my name: ')
-print('[Input] Yes or No')
-Sure=input(CompName, 'Are you sure:')
-if(Sure.__contains__('Yes')):
-  print('Alright, sounds good')
-elif(Sure.__contains__('No')):
-  print()
-  app.stop()
+print(Name, 'Thats a nice name. I wish I had one....')
+print('HEY! Ive got an idea! Why dont you give me a name?')
+CompName=input('What would you like to call me: ')
+print(CompName, 'sounds like a cool name.')
 print('What is your favorite number?')
 Number=input('Number: ')
 print('I like', Number, 'Too!')
-print('What do you think about math?')
-print('[Input] If you like math, input 1. If not, input 2.')
-Math=input('Math: ')
-if(Math.__contains__('1')):
-  print('Good for you! Im not good at math.')
-if(Math.__contains__('2')):
-  print('Same here! Im just not good at math XD.')
+#print('What do you think about math?')
+#print('[Input] If you like math, input 1. If not, input 2.')
+#Math=input('Math: ')
+#if(Math.__contains__('1')):
+#  print('Good for you! Im not good at math.')
+#elif(Math.__contains__('2')):
+#  print('Same here! Im just not good at math XD.')
+print('Well it was nice to meet you', Name, 'I hope we meet again soon')
